5b3c6e8f09812328513b158df82ebeb802eadea6
Rajath K <49567042+Mikedemoose@users.noreply.github.com>
2026-08-25 21:17:50 +0530

    Add: longest-common-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-0.go
@@ -0,0 +1,28 @@
+func longestCommonSubsequence(text1 string, text2 string) int {
+    dp := make([][]int, len(text1)+1)
+	for i := range len(text1)+1 {
+		dp[i] = make([]int, len(text2)+1)
+	}
+	longestSubsequenceLength := 0
+
+	for i:=1; i<len(text1)+1; i++ {
+
+
+		for j:=1; j<len(text2)+1; j++ {
+
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = max(dp[i-1][j-1]+1, dp[i-1][j], dp[i][j-1])
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+
+			if dp[i][j] > longestSubsequenceLength {
+				longestSubsequenceLength = dp[i][j]
+			}
+
+		}
+
+	}
+
+	return longestSubsequenceLength
+}
