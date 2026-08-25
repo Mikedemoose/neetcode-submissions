cbb452946663b7d94de615a0f763d2942546026b
Rajath K <49567042+Mikedemoose@users.noreply.github.com>
2026-08-25 17:53:18 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.go
@@ -0,0 +1,14 @@
+func maxProfit(prices []int) int {
+	maxProfit := 0
+
+	for i:=1; i<len(prices); i++ {
+		if prices[i-1] < prices[i] {
+			if prices[i]-prices[i-1] > maxProfit {
+				maxProfit = prices[i]-prices[i-1]
+			}
+			prices[i] = prices[i-1]
+		} 
+	}
+
+	return maxProfit
+}
