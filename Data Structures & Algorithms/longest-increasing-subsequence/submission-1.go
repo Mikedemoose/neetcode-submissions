00560309027a5ecefad22ed96a8fc65595f442e2
Rajath K <49567042+Mikedemoose@users.noreply.github.com>
2026-08-25 09:19:23 +0530

    Add: longest-increasing-subsequence - submission-1

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-1.go
@@ -0,0 +1,23 @@
+func lengthOfLIS(nums []int) int {
+    lisArray := make([]int, len(nums))
+
+	lisArray[0] = 1
+
+	maxVal := 1
+
+	for i:=1; i<len(nums); i++ {
+		lisArray[i] = 1
+
+		for j:=i-1; j>=0; j-- {
+			if (nums[j] < nums[i]) && (lisArray[j] >= lisArray[i]) {
+				lisArray[i] = lisArray[j] + 1
+			}
+
+		}
+		if lisArray[i] > maxVal {
+			maxVal = lisArray[i]
+		}
+	}
+
+	return maxVal
+}
