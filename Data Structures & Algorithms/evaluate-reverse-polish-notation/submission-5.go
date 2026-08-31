be5d5132fadfb9c57d876ad2214fee22a092c670
Rajath K <49567042+Mikedemoose@users.noreply.github.com>
2026-08-31 20:32:22 +0530

    Add: evaluate-reverse-polish-notation - submission-5

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-5.go
@@ -0,0 +1,37 @@
+func evalRPN(tokens []string) int {
+	stack := make([]int, 0)
+	if a, ok := isOperand(tokens[0]); !ok {
+		stack = append(stack, a)
+	}
+
+	for i:=1; i<len(tokens); i++ {
+		if a, ok := isOperand(tokens[i]); !ok {
+			stack = append(stack, a)
+			continue
+		}
+		a, b := stack[len(stack)-2], stack[len(stack)-1]
+		stack = stack[:len(stack)-2]
+		stack = append(stack, doOperation(a, b, tokens[i]))
+	}
+
+	return stack[0]
+}
+
+func isOperand(a string) (int, bool) {
+	if a == "+" || a == "-" || a == "*" || a == "/" {
+		return 0, true
+	}
+	val, _ := strconv.Atoi(a)
+	return val, false
+}
+
+func doOperation(a, b int, op string) int {
+	if op == "+" {
+		return a+b
+	} else if op == "-" {
+		return a-b
+	} else if op == "*" {
+		return a*b
+	} 
+	return a/b
+}
