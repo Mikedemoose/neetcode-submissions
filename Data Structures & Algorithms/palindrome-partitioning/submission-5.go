38332a30a25e96f6670cf7b404bcbcd673acafef
Rajath K <49567042+Mikedemoose@users.noreply.github.com>
2026-09-16 21:57:51 +0530

    Add: palindrome-partitioning - submission-5

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-5.go
@@ -0,0 +1,29 @@
+func partition(s string) [][]string {
+	finalRes := make([][]string, 0)
+
+	for i:=0; i<len(s)-1; i++ {
+		if checkIfPalindrome(s[:i+1]) {
+			restPartitions := partition(s[i+1:])
+			for _, item := range restPartitions {
+				finalRes = append(finalRes, append([]string{s[:i+1]}, item...))
+			}
+		}
+	}
+	if len(s) > 0 && checkIfPalindrome(s) {
+		finalRes = append(finalRes, []string{s})
+	}
+	return finalRes
+
+}
+
+func checkIfPalindrome(s string) bool {
+	left, right := 0, len(s)-1
+	for left < right {
+		if s[left] != s[right] {
+			return false
+		}
+		left++
+		right--
+	}
+	return true
+}
