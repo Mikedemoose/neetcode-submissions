f0b228eee1ab1503cf021613aca16da44eee6b74
Rajath K <49567042+Mikedemoose@users.noreply.github.com>
2026-09-23 14:38:06 +0530

    Add: merge-triplets-to-form-target - submission-0

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-0.go
@@ -0,0 +1,13 @@
+func mergeTriplets(triplets [][]int, target []int) bool {
+    x, y, z := -1, -1, -1
+	for _, item := range triplets {
+		allowMerge := (item[0] <= target[0]) && (item[1] <= target[1]) && (item[2] <= target[2])
+		if allowMerge {
+			x = max(x, item[0])
+			y = max(y, item[1])
+			z = max(z, item[2])
+		}
+	}
+
+	return (x==target[0]) && (y==target[1]) && (z==target[2])
+}
