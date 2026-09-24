ec401f5f5cdf0b61b0832d7446a517054fd807de
Rajath K <49567042+Mikedemoose@users.noreply.github.com>
2026-09-24 21:57:52 +0530

    Add: cheapest-flight-path - submission-6

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-6.go
@@ -0,0 +1,47 @@
+func findCheapestPrice(n int, flights [][]int, src int, dst int, k int) int {
+	costMap := make(map[int][]QueueItem)
+	for _, flight := range flights {
+		costMap[flight[0]] = append(costMap[flight[0]], QueueItem{
+			curr: flight[1],
+			cost: flight[2],
+		})
+	}
+
+	queue := []QueueItem { QueueItem { curr: src }}
+
+	cheapestCost := make([]int, 100)
+	for i := range 100 {
+		cheapestCost[i] = -1
+	}
+
+	for range k+2 {
+		newQueue := make([]QueueItem, 0)
+		for _, item := range queue {
+			if cheapestCost[item.curr] != -1 && cheapestCost[item.curr] < item.cost {
+				continue
+			} else {
+				cheapestCost[item.curr] = item.cost
+			}
+
+			next := costMap[item.curr]
+
+			for _, nextStop := range next {
+				newQueue = append(newQueue, QueueItem{
+					curr: nextStop.curr,
+					cost: item.cost + nextStop.cost,
+				})
+
+			}
+
+		}
+		queue = newQueue
+	}
+
+	return cheapestCost[dst]
+}
+
+
+type QueueItem struct {
+	curr int
+	cost int
+}
