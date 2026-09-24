b20c4c7e93affc3058d726e72f1fc89040684326
Rajath K <49567042+Mikedemoose@users.noreply.github.com>
2026-09-24 20:41:01 +0530

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.go
@@ -0,0 +1,37 @@
+func letterCombinations(digits string) []string {
+	if len(digits) == 0 {
+		return make([]string, 0)
+	}
+
+	digitMap := map[byte] []byte {
+		'2': []byte{'a', 'b', 'c'},
+		'3': []byte{'d', 'e', 'f'},
+		'4': []byte{'g', 'h', 'i'},
+		'5': []byte{'j', 'k', 'l'},
+		'6': []byte{'m', 'n', 'o'},
+		'7': []byte{'p', 'q', 'r', 's'},
+		'8': []byte{'t', 'u', 'v'},
+		'9': []byte{'w', 'x', 'y', 'z'},
+	}
+
+	queue := []string {""}
+
+	for i := range len(digits) {
+
+		currLetter := digits[i]
+		chars := digitMap[currLetter]
+		newQueue := make([]string, 0)
+
+		for _, char := range chars {
+			for _, item := range queue {
+				newQueue = append(newQueue, item + string(char))
+			}
+		}
+
+		queue = newQueue
+
+	}
+
+	return queue
+	
+}
